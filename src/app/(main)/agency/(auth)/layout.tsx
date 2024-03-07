93cbf55c5f31b5b45293cbb6e7b80dae9dfcb513
Adarsh chhetri <chhetriadarsh912@gmail.com>
2024-03-07 09:08:46 -0800

    tFix route issue

--- src/app/(main)/agency/(auth)/layout.tsx
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
 
-const layout = ({children}: {children: React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full flex items-center justify-center'>
+    <div className="h-full flex items-center justify-center text-white ">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Page = () => {
-  return <div></div>;
+const page = () => {
+  return <div>Domain - page</div>;
 };
 
-export default Page;
+export default page;
